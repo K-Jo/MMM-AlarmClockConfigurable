f570a6acddb18568b09f9c0cc1c591401565e565
Pieter Cailliau <pieter.cailliau@neotechnology.com>
2017-09-02 15:51:01 +0100

    wrong port

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var minutes = 5;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
