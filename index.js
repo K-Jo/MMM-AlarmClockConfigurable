b4a60895e177bd522dbd211f1d2be3707d938b28
Pieter Cailliau <pieter.cailliau@neotechnology.com>
2017-09-02 12:27:01 +0100

    adding basic service and time picker

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
+var express = require('express')
 
 var serve = serveStatic("./static");
 
@@ -11,3 +12,13 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(8000);
+
+
+var app = express()
+var alarm = false;
+
+app.get('/status', function (req, res) {
+  res.send(alarm)
+})
+
+app.listen(8001)
