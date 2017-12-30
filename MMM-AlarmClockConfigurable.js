ebe88a6e7b05777db8e5062b0a241f3b3aafb180
Pieter Cailliau <pieter.cailliau@neotechnology.com>
2017-12-30 20:09:28 +0000

    first attempt for button

--- MMM-AlarmClockConfigurable.js
+++ MMM-AlarmClockConfigurable.js
@@ -13,7 +13,7 @@ Module.register('MMM-AlarmClockConfigurable', {
 
     defaults: {
         sound: 'alarm.mp3',
-        touch: false,
+        touch: true,
         volume: 1.0,
         format: 'ddd, h:mmA',
         timer: 60 * 1000 // one minute
@@ -45,37 +45,14 @@ Module.register('MMM-AlarmClockConfigurable', {
     checkAlarm() {
       if(!this.alarmFired){
         if(this.next && moment().diff(this.next.moment) >= 0){
-          const alert = {
-              imageFA: 'bell-o',
-              title: this.next.sender || this.next.title,
-              message: this.next.message
-          };
-          if (!this.config.touch) {
-              alert.timer = this.config.timer;
-          }
-          this.sendNotification('SHOW_ALERT', alert);
-          this.alarmFired = true;
-          this.updateDom(300);
-          this.timer = setTimeout(() => {
-              this.resetAlarmClock();
-          }, this.config.timer);
-          if (this.config.touch) {
-              MM.getModules().enumerate((module) => {
-                  if (module.name === 'alert') {
-                      module.alerts['MMM-AlarmClockConfigurable'].ntf.addEventListener('click', () => {
-                          clearTimeout(this.timer);
-                          this.resetAlarmClock();
-                      });
-                  }
-              });
-          }
+          this.fireAlarm();
         } else {
-          this.getNextAlarm();
+          this.getNextAlarmTime();
         }
       }
     },
 
-    getNextAlarm() {
+    getNextAlarmTime() {
       var self = this;
       var alarmRequest = new XMLHttpRequest();
       alarmRequest.open("GET", "http://bedroompi:8001/alarm/time/next", true);
@@ -93,39 +70,39 @@ Module.register('MMM-AlarmClockConfigurable', {
       alarmRequest.send();
     },
 
-    resetAlarmClock() {
-        this.alarmFired = false;
-        if (this.config.touch) {
-            this.sendNotification('HIDE_ALERT');
-        }
-        this.getNextAlarm();
+    fireAlarm() {
+      // const alert = {
+      //     imageFA: 'bell-o',
+      //     title: this.next.sender || this.next.title,
+      //     message: this.next.message
+      // };
+      // if (!this.config.touch) {
+      //     alert.timer = this.config.timer;
+      // }
+      // this.sendNotification('SHOW_ALERT', alert);
+      this.alarmFired = true;
+      this.updateDom(300);
+      // this.timer = setTimeout(() => {
+      //     this.resetAlarmClock();
+      // }, this.config.timer);
+      // if (this.config.touch) {
+      //     MM.getModules().enumerate((module) => {
+      //         if (module.name === 'alert') {
+      //             module.alerts['MMM-AlarmClockConfigurable'].ntf.addEventListener('click', () => {
+      //                 clearTimeout(this.timer);
+      //                 this.resetAlarmClock();
+      //             });
+      //         }
+      //     });
+      // }
     },
 
-    getMoment(alarm) {
-        const now = moment();
-        let difference = Math.min();
-        const hour = parseInt(alarm.time.split(':')[0]);
-        const minute = parseInt(alarm.time.split(':')[1]);
-
-        for (let i = 0; i < alarm.days.length; i += 1) {
-            if (now.day() < alarm.days[i]) {
-                difference = Math.min(alarm.days[i] - now.day(), difference);
-            } else if (now.day() === alarm.days[i] && (parseInt(now.hour()) < hour ||
-                (parseInt(now.hour()) === hour && parseInt(now.minute()) < minute))) {
-                difference = Math.min(0, difference);
-            } else if (now.day() === alarm.days[i]) {
-                difference = Math.min(7, difference);
-            } else {
-                difference = Math.min((7 - now.day()) + alarm.days[i], difference);
-            }
-        }
-
-        return moment().add(difference, 'days').set({
-            hour,
-            minute,
-            second: 0,
-            millisecond: 0
-        });
+    resetAlarmClock() {
+        this.alarmFired = false;
+        // if (this.config.touch) {
+        //     this.sendNotification('HIDE_ALERT');
+        // }
+        this.getNextAlarmTime();
     },
 
     getDom() {
@@ -158,6 +135,11 @@ Module.register('MMM-AlarmClockConfigurable', {
             sound.setAttribute('autoplay', true);
             sound.setAttribute('loop', true);
             wrapper.appendChild(sound);
+
+            const stopalarm = document.createElement('i');
+            stopalarm.classList.add('fa', 'fa-bell-slash', 'logo');
+            stopalarm.addEventListener('click', () => { this.resetAlarmClock(); });            
+            wrapper.appendChild(stopalarm);
         } else {
             const alarm = document.createElement('div');
             alarm.classList.add('small');
@@ -166,5 +148,32 @@ Module.register('MMM-AlarmClockConfigurable', {
         }
 
         return wrapper;
+    },
+
+    getMoment(alarm) {
+        const now = moment();
+        let difference = Math.min();
+        const hour = parseInt(alarm.time.split(':')[0]);
+        const minute = parseInt(alarm.time.split(':')[1]);
+
+        for (let i = 0; i < alarm.days.length; i += 1) {
+            if (now.day() < alarm.days[i]) {
+                difference = Math.min(alarm.days[i] - now.day(), difference);
+            } else if (now.day() === alarm.days[i] && (parseInt(now.hour()) < hour ||
+                (parseInt(now.hour()) === hour && parseInt(now.minute()) < minute))) {
+                difference = Math.min(0, difference);
+            } else if (now.day() === alarm.days[i]) {
+                difference = Math.min(7, difference);
+            } else {
+                difference = Math.min((7 - now.day()) + alarm.days[i], difference);
+            }
+        }
+
+        return moment().add(difference, 'days').set({
+            hour,
+            minute,
+            second: 0,
+            millisecond: 0
+        });
     }
 });
